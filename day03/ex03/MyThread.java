d43e11b8596e067408651c4c396a68c7892897dc
Bail Agda <bailagda@student.21-school.ru>
2023-04-11 19:01:39 +0300

    fix downloading files

--- day03/ex03/MyThread.java
+++ day03/ex03/MyThread.java
@@ -2,6 +2,7 @@ package ex03;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -19,15 +20,24 @@ public class MyThread extends Thread{
         try {
             for(;;) {
                 String s = reader.readLine();
-                if (s != null){
+                if (s != null) {
                     String[] fileUrl = s.split(" ");
+                    String name = s.substring(s.lastIndexOf("/") + 1);
                     System.out.printf("%s start download file number %s\n", this.getName(), fileUrl[0]);
                     BufferedInputStream in = new BufferedInputStream(new URL(fileUrl[1]).openStream());
-                    Files.copy(in, Paths.get("/Users/bailagda/Desktop/java_bootcamp/day03/ex03/test/"),
-                            StandardCopyOption.REPLACE_EXISTING);
+//                    Files.copy(in, Paths.get("/Users/bailagda/Desktop/java_bootcamp/day03/ex03/test/"),
+//                            StandardCopyOption.REPLACE_EXISTING);
+                    FileOutputStream fos = new FileOutputStream(
+                            "/Users/bailagda/Desktop/java_bootcamp/day03/ex03/test/"+ name);
+                    byte[] data = new byte[1024];
+                    int count;
+                    while ((count = in.read(data, 0, 1024)) != -1) {
+                        fos.write(data, 0, count);
+                    }
                     System.out.printf("%s finish download file number %s\n", this.getName(), fileUrl[0]);
                 } else {
                     break;
+
                 }
             }
         } catch (IOException e) {
@@ -35,3 +45,5 @@ public class MyThread extends Thread{
         }
     }
 }
+
+
