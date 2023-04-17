4ffe7061af382a5b4cef40103ce9fafc55188d49
Bail Agda <bailagda@student.21-school.ru>
2023-04-17 18:45:43 +0300

    fix hierarсhy

--- day05/ex00/Chat/src/main/resources/data.sql
+++ day05/ex00/Chat/src/main/resources/data.sql
@@ -11,15 +11,15 @@ VALUES
 INSERT INTO chatrooms_table(name, owner)
 VALUES
      ('Chat_1', (SELECT id FROM users_table WHERE login = 'milkfist')),
-     ('Chat_2', (SELECT id FROM users_table WHERE login = 'gabriela')),
-     ('Chat_3', (SELECT id FROM users_table WHERE login = 'huongchi')),
-     ('Chat_4', (SELECT id FROM users_table WHERE login = 'stevenso')),
-     ('Chat_5', (SELECT id FROM users_table WHERE login = 'dedelmir'));
+    ('Chat_2', (SELECT id FROM users_table WHERE login = 'gabriela')),
+    ('Chat_3', (SELECT id FROM users_table WHERE login = 'huongchi')),
+    ('Chat_4', (SELECT id FROM users_table WHERE login = 'stevenso')),
+    ('Chat_5', (SELECT id FROM users_table WHERE login = 'dedelmir'));
 
-INSERT INTO messages_table(author, room, text, message_date)
+INSERT INTO messages_table(author_id, room_id, text, message_date)
 VALUES
-    ((SELECT id FROM chat.users WHERE login = 'dedelmir'), 1, 'Hi, Chat_1', 2023-03-20 11:23:44),
-    ((SELECT id FROM chat.users WHERE login = 'stevenso'), 2, 'Hi, Chat_2', 2023-03-20 12:23:44),
-    ((SELECT id FROM chat.users WHERE login = 'huongchi'), 3, 'Hi, Chat_3', 2023-03-20 13:23:44),
-    ((SELECT id FROM chat.users WHERE login = 'gabriela'), 4, 'Hi, Chat_4', 2023-03-20 14:23:44),
-    ((SELECT id FROM chat.users WHERE login = 'milkfist'), 5, 'Hi, Chat_5', 2023-03-20 15:23:44);
+    ((SELECT id FROM users_table WHERE login = 'dedelmir'), 1, 'Hi, Chat_1', (SELECT NOW()::timestamp)),
+    ((SELECT id FROM users_table WHERE login = 'stevenso'), 2, 'Hi, Chat_2', (SELECT NOW()::timestamp)),
+    ((SELECT id FROM users_table WHERE login = 'huongchi'), 3, 'Hi, Chat_3', (SELECT NOW()::timestamp)),
+    ((SELECT id FROM users_table WHERE login = 'gabriela'), 4, 'Hi, Chat_4', (SELECT NOW()::timestamp)),
+    ((SELECT id FROM users_table WHERE login = 'milkfist'), 5, 'Hi, Chat_5', (SELECT NOW()::timestamp));
--- day05/ex00/Chat/src/main/resources/schema.sql
+++ day05/ex00/Chat/src/main/resources/schema.sql
@@ -1,24 +1,21 @@
 --CREATE TABLE
 
-CREATE TABLE [IF NOT EXISTS] users_table (
-   id serial PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS users_table (
+   id serial PRIMARY KEY,
    Login VARCHAR ( 50 ) UNIQUE NOT NULL,
-   Password VARCHAR ( 50 ) NOT NULL,
-   rooms_own INTEGER,
-   socializes INTEGER
+   Password VARCHAR ( 50 ) NOT NULL
 );
 
-CREATE TABLE [IF NOT EXISTS] chatrooms_table (
-   id serial PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS chatrooms_table (
+   id serial PRIMARY KEY,
    name VARCHAR ( 50 ) UNIQUE NOT NULL,
-   owner INTEGER,
-   message_id INTEGER
+   owner INTEGER references users_table(id)
 );
 
-CREATE TABLE [IF NOT EXISTS] messages_table (
-   id serial PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-   author INTEGER,
-   room INTEGER,
-   text VARCHAR,
-   message_date DATE
+CREATE TABLE IF NOT EXISTS messages_table (
+   id serial PRIMARY KEY,
+   author_id INTEGER references users_table(id),
+   room_id INTEGER references chatrooms_table(id),
+   text VARCHAR not null ,
+   message_date timestamp
 );
