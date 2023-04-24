8fe511ec52067353476e3cf09ddcb0a030965601
bailagda <skseniya907@gmail.com>
2023-04-25 01:04:35 +0400

    need work on postgre.org

--- day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -3,18 +3,11 @@ package src.main.java.edu.school21.chat.app;
 import src.main.java.edu.school21.chat.models.Message;
 import src.main.java.edu.school21.chat.repositories.MessagesRepository;
 import src.main.java.edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
-
-import javax.sql.DataSource;
-
 import java.sql.SQLException;
 import java.util.Optional;
 
-import static com.sun.xml.internal.ws.encoding.xml.XMLMessage.getDataSource;
-
 public class Program {
     public static void main(String[] args) throws SQLException {
-        DataSource ds = (DataSource) getDataSource(null, null);
-
         MessagesRepositoryJdbcImpl msgRepo = new MessagesRepositoryJdbcImpl();
         Optional<Message> res = msgRepo.findById(1L);
         System.out.print(res);
--- day05/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
@@ -2,33 +2,42 @@ package src.main.java.edu.school21.chat.repositories;
 
 import src.main.java.edu.school21.chat.models.Message;
 
-import javax.sound.midi.Soundbank;
-import javax.sql.DataSource;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.Optional;
 
 public class MessagesRepositoryJdbcImpl implements MessagesRepository {
-//    private final DataSource ds;
+    private static final String url = "jdbc:postgresql://localhost:5433/first_db";
+    private static final String username = "postgres";
+    private static final String password = "123";
+    static Connection conn;
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-//    public MessagesRepositoryJdbcImpl(DataSource ds) {
-//        this.ds = ds;
-//    }
 
     @Override
     public Optional<Message> findById(Long id) {
-        Optional<Message> opM = null;
-            String url = "jdbc:postgresql://localhost:5432/postgres";
-            String username = "root";
-            String password = "password";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
 
-            Connection conn = DriverManager.getConnection(url, username, password);
+        Optional<Message> opM = null;
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
 
             Statement statement = conn.createStatement();
+            String sql = "SELECT * FROM messages_table WHERE id =";
 
-            ResultSet rs = statement.executeQuery("SELECT * FROM messages_table WHERE id =" + id);
+            ResultSet rs = statement.executeQuery(sql + id);
+            while(rs.next()) {
+                System.out.println(rs);
+            }
             opM = Optional.of(new Message(rs.getInt(1), null, null, null, null));
 
 
