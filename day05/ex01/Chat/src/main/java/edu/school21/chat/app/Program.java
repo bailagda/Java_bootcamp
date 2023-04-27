9cbabcbff64d3de7d15be42ce37a922f50a6be87
Bail Agda <bailagda@student.21-school.ru>
2023-04-27 20:41:33 +0300

    finish ex01 without hikaru

--- day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -1,8 +1,7 @@
-package src.main.java.edu.school21.chat.app;
+package edu.school21.chat.app;
 
-import src.main.java.edu.school21.chat.models.Message;
-import src.main.java.edu.school21.chat.repositories.MessagesRepository;
-import src.main.java.edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
+import edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
+import edu.school21.chat.models.Message;
 import java.sql.SQLException;
 import java.util.Optional;
 
