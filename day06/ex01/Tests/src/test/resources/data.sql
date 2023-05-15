9ecc5293ef11ddd3d29e1fe87b0123785815f6d2
Kseniya Sidorova <bailagda@student.21-school.ru>
2023-05-15 20:23:29 +0300

    Update data.sql

---
+++ day06/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (id, name, price) VALUES (1, 'abra-cadabra', 80);
+INSERT INTO product (id, name, price) VALUES (2, 'kalyaka-balyaka', 50);
+INSERT INTO product (id, name, price) VALUES (3, 'syaski-masyaski',100);
